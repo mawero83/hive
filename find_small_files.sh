bb94e5df833e2ed482498939e6785d158bf08b53
Marco Aguero <44516809+mawero83@users.noreply.github.com>
2024-01-29 11:20:48 -0500

    Update find_small_files.sh

--- find_small_files.sh
+++ find_small_files.sh
@@ -3,30 +3,43 @@
 # Define the size threshold for 'small' files in bytes
 # For example, 1000000 for files smaller than 1MB
 SMALL_SIZE_THRESHOLD=1000000
-
 # Function to count small files in a given HDFS path
 count_small_files() {
     local path=$1
     hadoop fs -ls $path | awk -v threshold=$SMALL_SIZE_THRESHOLD '
     BEGIN { count = 0; }
     {
-        if ($5 < threshold) { 
-            count++; 
+        if ($5 < threshold) {
+            count++;
         }
     }
     END { print count; }'
 }
 
 # Get the list of tables from Hive
-TABLES=$(hive -S -e 'SHOW TABLES;')
+
+IFS="
+"
+DBS=$(hive -e 'SHOW DATABASES;')
+DBS=`echo "$DBS"|sed 's/+//g;s/|//g;s/ //g;s/-//g'|grep -v database_name| strings`
+echo "--------------------"
+echo "$DBS"
+echo "--------------------"
 
 # Iterate over each table
-for table in $TABLES; do
+for db in `echo "$DBS"`; do
     # Get the HDFS location of the table
-    LOCATION=$(hive -S -e "DESCRIBE FORMATTED $table;" | grep 'Location:' | awk '{print $NF}')
-    
+    TABLES=$(hive -e 'SHOW TABLES;')
+    TABLES=`echo "$TABLES"|sed 's/+//g;s/|//g;s/ //g;s/-//g'|grep -v tab_name| strings`
+    for table in `echo "$TABLES"`;  do
+    set -x
+    LOCATION=$(hive -e "DESCRIBE FORMATTED ${db}.${table};" | grep 'Location:' | awk '{print $4}')
+
     # Count small files in this location
     SMALL_FILES=$(count_small_files $LOCATION)
 
     echo "Table: $table, Small Files: $SMALL_FILES"
+    set +x
+    done
 done
+
